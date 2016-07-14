c2e3a7aece12186f97a89a97436e7151de41375e
AlexKrasnov <ya.sasha-krasnov@yandex.ru>
2016-07-15 02:09:11 +0300

    class ListElement

---
+++ include/ListElement.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MatrixElement.h"
+
+// класс одного элемента списка
+class ListElement
+{
+	MatrixElement a; // элемент матрицы
+	ListElement* next; // следующий элемент списка
+	friend class MatrixElement;
+	friend class MatrixList;
+public:
+	// конструктор одного элемента списка
+	ListElement(int i1, int j1 , double val) : a(i1,j1,val), next(NULL) {}
+	MatrixElement GetElement() const { return a; }
+	ListElement* GetNext() const { return next; }
+	friend ostream& operator << (ostream& out, ListElement& ob)
+	{
+		out << ob.a;
+		return out;
+	}
+};
